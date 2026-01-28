632915ada1ad51b52c924b970f75e9fbf5bc5801
Himanshu <iamhims738@gmail.com>
2026-01-28 13:51:56 +0530

    Initial commit: Supply Chain SaaS Platform with 7 microservices

---
+++ Backend/Services/Billing-service/src/test/java/com/supplychain/Billing_service/BillingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supplychain.Billing_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Backend/Services/exception-service/src/test/java/com/supplychain/exception_service/ExceptionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supplychain.exception_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
